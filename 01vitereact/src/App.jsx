6df1277a03a6a6894046a664987391e8ecf17e1d
= <ashlesha.deshpande@skf.com>
2024-06-25 07:38:29 +0530

    input changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,20 +1,36 @@
 import Chai from "./Chai";
 import User from "./User";
-import Student from "./Student"
-import React,{useState} from "react";
+import Student from "./Student";
+import React, { useState } from "react";
 import Test from "./Test";
+
 function App() {
+  const [name, setName] = useState("abc");
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
 
-  const [name, setName] = useState("abc")
+  function getData(val) {
+    console.warn(val.target.value);
+    setData(val.target.value); //value dikhane ke liye
+    
+  }
 
-  
   return (
     <>
       <Chai />
       <User />
       <Student name={name} email={"abc@gmail.com"} />
-      <button onClick={()=>{setName("XYZ")}}>Update name</button>
-      <Test/>
+      <button
+        onClick={() => {
+          setName("XYZ");
+        }}
+      >
+        Update name
+      </button>
+      <Test />
+      {print ? <h1>{data}</h1> : null}
+      <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>Print</button>
     </>
   );
 }
