e751aee93cc424c6f696df40ce8daef3c8e5f1e2
= <ashlesha.deshpande@skf.com>
2024-06-25 18:30:41 +0530

    bootstrap for list

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import {Button} from 'react-bootstrap'
+import { Table } from "react-bootstrap";
 
 function App() {
+  const users = [
+    { name: "Anil", email: "anil@test.com", contact: "111" },
+    { name: "Burce", email: "bruce@test.com", contact: "222" },
+    { name: "Peter", email: "peter@test.com", contact: "111" },
+    { name: "Sam", email: "sam@test.com", contact: "777" },
+  ];
   return (
     <div className="App">
-      <h1>Bootstrap</h1>
-      <Button onClick={()=>alert("bootstrap is working fine")}>click me</Button>
+      <h1>List with bootstrap</h1>
+      <Table striped bordered hover variant="dark">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Contact</td>
+          </tr>
+          {users.map((item, i) => (
+            item.contact === '111'?
+            <tr key={i}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.contact}</td>
+              </tr>
+              :null
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
