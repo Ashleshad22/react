8bfaeadc61e28b486e89d15748fd4b24921f5d1c
= <ashlesha.deshpande@skf.com>
2024-06-26 11:46:07 +0530

    readme changes

--- 01vitereact/src/Student.jsx
+++ 01vitereact/src/Student.jsx
@@ -1,10 +1,10 @@
 function Student(props) {
   console.log(props.name);
   return (
-    <div>
+    <>
       <h1>Hello {props.name}</h1>
       <h2>Email: {props.email}</h2>
-    </div>
+    </>
   );
 }
 
